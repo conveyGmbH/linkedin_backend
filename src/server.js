699d301ce46ee986e01ec80a23909944934e8096
Hung <quanghungbui1@gmail.com>
2025-11-24 16:02:44 +0100

    test routing

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+﻿const express = require('express');
 const path = require('path');
 const app = express();
 const TokenStore = require('./token-store.js');
@@ -28,6 +28,10 @@ if (!clientId || !clientSecret) {
 
 // Statische Dateien bereitstellen (index.html)
 app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.end(JSON.stringify({ a: 1 }, null, 3));
+});
 
 // Callback-Route für LinkedIn
 app.get('/auth/callback', (req, res) => {
